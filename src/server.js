d86c7dd9a1eacfb8232879ffeec11b9950db9d96
skywarth <yigitk.ersoy@gmail.com>
2023-11-26 20:27:02 +0300

    revert dotenv changes. Render.com rules

--- src/server.js
+++ src/server.js
@@ -1,9 +1,5 @@
+require('dotenv').config();
 
-//require('dotenv').config();
-
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-}
 
 const express = require('express');
 const axios = require('axios');
